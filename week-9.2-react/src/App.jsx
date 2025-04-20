ba3db95e85d4868078342768f6f157c72d8f5b00
vineet <vineet8.4e8@gmail.com>
2025-04-20 22:09:11 +0530

    stop-watch_codebaseSetup

---
+++ week-9.2-react/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect} from "react";
+
+function App() {
+  return <div>
+    <Counter></Counter>
+  </div>
+}
+
+
+// mounting, re-rendering, unmounting
+function Counter(){
+
+  // const [state, setState] = useState(initialValue); syntax
+  const [count, setCount] = useState(0);
+
+
+  // hooking into the lifecycle events of react
+  console.log("Counter");
+
+  // Guard our setInterval from re-rendering
+  useEffect(function(){
+    setInterval(function(){
+      setCount(count => count + 1);
+    },1000)
+    console.log("mounted");
+  }, [])
+
+
+  return <div>
+    <h1 id="text">{count}</h1>
+  </div>
+}
+
+export default App
